7b2efae01e67d2e13c5026beac47c44d0cdf1474
Cliffano Subagio <cliffano@gmail.com>
2023-05-26 20:57:03 +1000

    Disable duplicate-code lint on test files.

--- tests-integration/test_json_conf.py
+++ tests-integration/test_json_conf.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring
+# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,duplicate-code
 import unittest
 from logconf.logconf import (
     get_logger,
--- tests/test_logconf.py
+++ tests/test_logconf.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring
+# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,duplicate-code
 from unittest.mock import patch
 import unittest.mock
 import unittest
