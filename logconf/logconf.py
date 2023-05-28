81f38e46cba7ac419516dcf67d94dfd4f975b4f3
Cliffano Subagio <cliffano@gmail.com>
2023-05-28 23:24:58 +1000

    Restructure logging, logger, and handlers' configuration parameters.

--- logconf/logconf.py
+++ logconf/logconf.py
@@ -1,29 +1,67 @@
-"""This module provides a function to get a logger
-with the specified name and configuration from conf_files.
+"""This module provides the Logconf class,
+which allows the management of Python logging
+via configuration files.
 """
 import logging
 from logconf.config import Config
 
-def get_logger(name, conf_files=None):
-    """Get a logger with the specified name
-    and configuration from conf_files.
+class Logconf():
+    """A class for managing Python logging logger and handlers.
     """
-    config = Config(conf_files=conf_files)
 
-    datefmt = config.get_datefmt()
-    filename = config.get_filename()
-    filemode = config.get_filemode()
-    _format = config.get_format()
-    level = config.get_level()
+    def __init__(self, conf_files=None):
+        """Initialise Python logging with configuration from conf_files.
+        """
 
-    file_handler = logging.FileHandler(filename, mode=filemode)
-    stream_handler = logging.StreamHandler()
+        config = Config(conf_files=conf_files)
+        handlers = config.get_handlers()
+        datefmt = config.get_datefmt()
+        _format = config.get_format()
+        level = config.get_level()
 
-    logging.basicConfig(
-        datefmt=datefmt,
-        format=_format,
-        level=level,
-        handlers=[stream_handler, file_handler]
-    )
+        self.handlers = []
+        if 'stream' in handlers:
+            self.handlers.append(self._init_stream_handler(config))
+        if 'file' in handlers:
+            self.handlers.append(self._init_file_handler(config))
 
-    return logging.getLogger(name)
+        logging.basicConfig(
+            datefmt=datefmt,
+            level=level
+        )
+
+    def _init_stream_handler(self, config):
+        """Initialise stream handler.
+        """
+        _format = config.get_format()
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter(_format))
+        return stream_handler
+
+    def _init_file_handler(self, config):
+        """Initialise file handler.
+        """
+        _format = config.get_format()
+        filename = config.get_filename()
+        filemode = config.get_filemode()
+        file_handler = logging.FileHandler(filename, mode=filemode)
+        file_handler.setFormatter(logging.Formatter(_format))
+        return file_handler
+
+    def get_logger(self, name):
+        """Get the logger based on the given name
+        and add the handlers to the logger.
+        """
+        logger = logging.getLogger(name)
+        for handler in self.handlers:
+            logger.addHandler(handler)
+        return logger
+
+    def close_handlers(self, name):
+        """Close logger handlers
+        and clear the handlers from logger.
+        """
+        logger = self.get_logger(name)
+        for handler in logger.handlers:
+            handler.close()
+        logger.handlers.clear()
--- tests/test_logconf.py
+++ tests/test_logconf.py
@@ -3,9 +3,7 @@ from unittest.mock import patch
 import unittest.mock
 import unittest
 import logging
-from logconf.logconf import (
-    get_logger,
-)
+from logconf.logconf import Logconf
 
 class TestLogconf(unittest.TestCase):
 
@@ -14,7 +12,7 @@ class TestLogconf(unittest.TestCase):
     @patch('logging.StreamHandler.__new__')
     @patch('logging.basicConfig')
     @patch('logging.getLogger')
-    def test_get_logger( # pylint: disable=too-many-arguments
+    def test_logconf( # pylint: disable=too-many-arguments
             self,
             func_get_logger,
             func_basic_config,
@@ -23,15 +21,16 @@ class TestLogconf(unittest.TestCase):
             func_config):
 
         mock_config = unittest.mock.Mock()
-        mock_file_handler = unittest.mock.Mock()
         mock_stream_handler = unittest.mock.Mock()
+        mock_file_handler = unittest.mock.Mock()
         mock_logger = unittest.mock.Mock()
 
         func_config.return_value = mock_config
-        func_file_handler.return_value = mock_file_handler
         func_stream_handler.return_value = mock_stream_handler
+        func_file_handler.return_value = mock_file_handler
         func_get_logger.return_value = mock_logger
 
+        mock_config.get_handlers.return_value = ['stream', 'file']
         mock_config.get_datefmt.return_value = '%d-%b-%y %H:%M:%S'
         mock_config.get_filename.return_value = 'logconf.log'
         mock_config.get_filemode.return_value = 'w'
@@ -39,13 +38,12 @@ class TestLogconf(unittest.TestCase):
                                               '%(name)s - %(levelname)s - %(message)s'
         mock_config.get_level.return_value = logging.INFO
 
-        result = get_logger('someloggername', conf_files=['somefile.ini', 'somefile.json'])
-        self.assertEqual(result, mock_logger)
+        logconf = Logconf(conf_files=['somefile.ini', 'somefile.json'])
+        logger = logconf.get_logger('someloggername')
+        self.assertEqual(logger, mock_logger)
 
         func_basic_config.assert_called_once_with(
             datefmt='%d-%b-%y %H:%M:%S',
-            format='%(asctime)s --> %(name)s - %(levelname)s - %(message)s',
             level=logging.INFO,
-            handlers=[mock_stream_handler, mock_file_handler]
         )
         func_get_logger.assert_called_once_with('someloggername')
